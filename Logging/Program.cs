84e85d51293574ab42566eafdbf250ddb948d50e
Mikael Chudinov <mikael.chudinov@datametrix.no>
2014-10-22 00:19:57 +0200

    Initial commit.

---
+++ Logging/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using log4net;
+
+namespace Logging
+{
+    class MainClass
+    {
+        static readonly ILog log = LogManager.GetLogger(typeof(MainClass));
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("sdf");
+            log.Info("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
